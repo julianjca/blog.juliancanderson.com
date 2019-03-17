89aa8d751a55622f02cbaabaa6de69624a4d7911
julianjca <julianjca11@gmail.com>
2019-03-17 13:44:07 +0700

    change bg color

--- src/components/layout.js
+++ src/components/layout.js
@@ -64,7 +64,7 @@ class Layout extends React.Component {
           marginRight: `auto`,
           maxWidth: rhythm(24),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-          backgroundColor: `#4C5D70`,
+          backgroundColor: `#36454F`,
           color: `#fff`
         }}
       >
